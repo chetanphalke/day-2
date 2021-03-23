da08c27b5634130c9cc3708dbc66a2aaf0926552
unknown <ichetanphalke@gmail.com>
2021-03-23 13:09:40 +0530

    commted by chetan

---
+++ Address.java
@@ -0,0 +1,11 @@
+
+public class Address {
+
+	int HouseNo;
+	int lane;
+	String Lanndmark;
+	String City;
+	enum state;
+	enum country;
+	
+}
---
+++ User.java
@@ -0,0 +1,14 @@
+import java.util.Date;
+
+public class User {
+
+	int id;
+	String FristName;
+	String LastName;
+	Date dob;
+	int height;
+	int waight;
+	int natianality;
+	Address address;
+	
+}
